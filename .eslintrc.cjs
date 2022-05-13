6f88ada10d6dc3eb9ca83df6f399895901e2aaa2
hiro <hiro@onuii.com>
2022-05-13 14:15:43 +0900

    eslint, prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
