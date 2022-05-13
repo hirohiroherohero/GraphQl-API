985bb14491d32d49d8ab27a7401a1a347705708c
hiro <hiro@onuii.com>
2022-05-13 19:19:19 +0900

    영화 api 구현

--- server.js
+++ server.js
@@ -1,1 +1,55 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
+
+const typeDefs = gql`
+
+
+  type Query {
+    allMovies: [Movie!]!
+    movie(id: String!): Movie
+  }
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
+  }
+`
+const resolvers = {
+  Query: {
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((res) => res.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((res) => res.json())
+        .then((json) => json.data.movie);
+    },
+  },
+
+}
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
